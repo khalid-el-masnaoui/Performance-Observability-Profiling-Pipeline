782df83a5b65d6b87ee30ae5bf3f7eb14523e7cf
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-09 12:12:00 +0100

    k6: ingest slow requests test -> test configs

--- k6/ingest_slow_requests.js
+++ k6/ingest_slow_requests.js
@@ -2,3 +2,13 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 const NGINX_URL = __ENV.NGINX_URL || "http://nginx";
+
+export const options = {
+  scenarios: {
+    steady_load: {
+      executor: 'constant-vus',
+      vus: 5,
+      duration: '2m',
+    },
+  },
+};

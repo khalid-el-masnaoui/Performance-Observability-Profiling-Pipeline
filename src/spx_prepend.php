fb9a4c41d6c782c0db4a8296704425e1eea8f331
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-10 12:12:00 +0100

    php : spx auto trigger prepend file

--- src/spx_prepend.php
+++ src/spx_prepend.php
@@ -7,3 +7,13 @@ $_SERVER['SPX_AUTO_START'] = '0';
 // ---------------------------
 $redis = new Redis();
 $redis->connect('redis');
+
+$route = strtok($_SERVER["REQUEST_URI"], '?');
+
+if ($redis->get("spx:$route")) {
+    echo "Request Profiled\n";
+    $_SERVER['SPX_AUTO_START'] = '1';
+    //ini_set('spx.http_enabled', '1');
+    spx_profiler_start();
+    register_shutdown_function('spx_profiler_stop');
+}

6037549a8b754f2c92f9790ecb8730b160f2c237
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-10 12:12:00 +0100

    php index file : import Prometheus and make redis connection

--- src/index.php
+++ src/index.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\APC;
+
+// ---------------------------
+// Redis (for SPX trigger)
+// ---------------------------
+$redis = new Redis();
+$redis->connect('redis');

99105fbea4e59e0ffa9e91dbdbabb2b6eaa94f05
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-11 12:12:00 +0100

    php flamegraphs viewer

--- src/flamegraphs.php
+++ src/flamegraphs.php
@@ -7,3 +7,15 @@ $q = $_GET['q'] ?? '';
 
 echo "<h1>SPX Flamegraphs</h1>";
 echo "<form><input name='q' placeholder='search route...'></form>";
+
+foreach ($files as $file) {
+    $name = basename($file);
+
+    if ($q && stripos($name, $q) === false) {
+        continue;
+    }
+
+    echo "<div style='margin:10px;padding:10px;border:1px solid #ccc'>";
+    echo "<a target='_blank' href='/spx-data/$name'>$name</a>";
+    echo "</div>";
+}

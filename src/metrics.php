77bbb4e3e13352ba90b60bf7c748a366e173e894
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-11 12:12:00 +0100

    php metrics file : APC storage

--- src/metrics.php
+++ src/metrics.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+use Prometheus\Storage\APC;
+
+// Same storage as index.php
+$registry = new CollectorRegistry(new APC());
